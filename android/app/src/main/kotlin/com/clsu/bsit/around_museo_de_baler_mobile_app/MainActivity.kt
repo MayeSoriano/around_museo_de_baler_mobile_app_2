5ae58c06a1bb0e14710fd605b2fbc4ec7dbeb046
MayeSoriano <154299152+MayeSoriano@users.noreply.github.com>
2024-11-28 19:58:08 +0800

    Add files via upload

---
+++ android/app/src/main/kotlin/com/clsu/bsit/around_museo_de_baler_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.clsu.bsit.around_museo_de_baler_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/around_museo_de_baler_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.clsu.bsit.aroundMuseoDeBalerMobileApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
